ba24ccab57a059f35ac6eaf52fb13e91846439de
Jose Medellin <kabo.0699@gmai.com>
2024-09-12 00:50:53 -0600

    server config added

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import app from './config/server.js';
+
+dotenv.config();
+const { PORT } = process.env;
+
+app.listen(PORT || 3300, () => {
+  console.log('ONLINE ', PORT);
+});
+
+export default app;
